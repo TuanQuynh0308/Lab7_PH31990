c16e8f1f46a3c6085be6c26f883b7af97269dfb8
TuanQuynh0308 <117746207+TuanQuynh0308@users.noreply.github.com>
2024-06-03 21:10:17 +0700

    v3

--- app/src/main/java/com/example/lab7_ph31990/MainActivity.kt
+++ app/src/main/java/com/example/lab7_ph31990/MainActivity.kt
@@ -1,47 +1,78 @@
 package com.example.lab7_ph31990
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab7_ph31990.ui.theme.Lab7_PH31990Theme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Lab7_PH31990Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val context = LocalContext.current
+        Button(
+            onClick = {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab7_PH31990Theme {
-        Greeting("Android")
+                val intent = Intent(context, Bai1Lab7::class.java)
+                context.startActivity(intent)
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Green,
+                contentColor = Color.White
+            )
+        ) {
+            Text("Bài 1")
+        }
+        Button(
+            onClick = {
+                val intent = Intent(context, Bai2Lab7::class.java)
+                context.startActivity(intent)
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Green,
+                contentColor = Color.White
+            )
+        ) {
+            Text("Bài 2")
+        }
+        Button(
+            onClick = {
+                val intent = Intent(context, Bai3Lab7::class.java)
+                context.startActivity(intent)
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Green,
+                contentColor = Color.White
+            )
+        ) {
+            Text("Bài 3")
+        }
     }
 }
